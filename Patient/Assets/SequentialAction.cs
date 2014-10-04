755730d742120760a21514a49110e4626a4b3483
Gregory Rose <greg@ncroses.com>
2014-10-04 18:07:51 -0400

    add initial game state skeleton code

---
+++ Patient/Assets/SequentialAction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SequentialAction : Action {
+
+	IList<Action> actions;
+	
+	// Run each action, waiting for each to be finished before the next one
+
+	public SequentialAction(IList<Action> actions) {
+		this.actions = actions;
+	}
+	
+	public void Start() {
+		// TODO: implement
+	}
+	
+}
