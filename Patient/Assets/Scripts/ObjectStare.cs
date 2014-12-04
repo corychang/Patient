f9a0240319c300adec3f43755885041241c40ded
csobel <Corey@Coreys-MBP.wv.cc.cmu.edu>
2014-12-04 17:26:32 -0500

    Added StareTrigger.cs and modified ObjectStare.cs. Added a sample stare state to the SampleScene

--- Patient/Assets/Scripts/ObjectStare.cs
+++ Patient/Assets/Scripts/ObjectStare.cs
@@ -1,16 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class ObjectStare : MonoBehaviour {
+	public delegate void StareHandler(string objectName);
+	const float TIME_FOR_STARE = 2.0f; // time required to stare at an object, in seconds
+
+
+
+	public event StareHandler OnStare;
+
+	private IDictionary<string, float> currentObjects;
+
 	public int angle = 10;
 	// Use this for initialization
 	void Start () {
-		// does nothing to initialize
+		currentObjects = new Dictionary<string, float> ();
+	}
+
+	void Update() {
+		float currentTime = Time.time;
+		IEnumerable<string> objectsHit = CastRays ();
+		// First add objects from objectsHit that aren't yet being tracked
+		foreach (string name in objectsHit) {
+			if (! currentObjects.ContainsKey(name)) {
+				currentObjects[name] = Time.time;
+			}
+		}
+
+		foreach (object obj in currentObjects.Keys.ToList()) {
+			string name = (string) obj;
+			if (objectsHit.Contains(name)) {
+				if (currentTime - currentObjects[name] > TIME_FOR_STARE) {
+					if (OnStare != null) {
+						OnStare(name);
+					}
+					// Require we stare at the object for TIME_FOR_STARE seconds before
+					// calling the trigger again. Just to stop us from spamming the fn call.
+					currentObjects.Remove(name);
+				}
+			} 
+			else {
+				currentObjects.Remove(name);
+			}
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	IEnumerable<string> CastRays () {
+		ArrayList objectsHit = new ArrayList ();
 		//new position and direction of camera
+		// TODO: Change from mainCamera to main
 		Vector3 pos = Camera.mainCamera.gameObject.transform.position;
 		Vector3 dir = Camera.mainCamera.gameObject.transform.forward;
 		Ray camVector = new Ray(pos, dir);
@@ -36,48 +77,30 @@ public class ObjectStare : MonoBehaviour {
 		Ray camVectorLowRight = new Ray(pos,dirLowRight);
 		Ray camVectorLowLeft = new Ray(pos,dirLowLeft);
 
-		Debug.DrawRay(pos, dir*10, Color.red);
-		Debug.DrawRay(pos, dirHigh*10, Color.red);
-		Debug.DrawRay(pos, dirLow*10, Color.red);
-		Debug.DrawRay(pos, dirRight*10, Color.red);
-		Debug.DrawRay(pos, dirLeft*10, Color.red);
-		Debug.DrawRay(pos, dirHighRight*10, Color.red);
-		Debug.DrawRay(pos, dirHighLeft*10, Color.red);
-		Debug.DrawRay(pos, dirLowRight*10, Color.red);
-		Debug.DrawRay(pos, dirLowLeft*10, Color.red);
+		Debug.DrawRay(pos, dir*100, Color.red);
+		Debug.DrawRay(pos, dirHigh*100, Color.red);
+		Debug.DrawRay(pos, dirLow*100, Color.red);
+		Debug.DrawRay(pos, dirRight*100, Color.red);
+		Debug.DrawRay(pos, dirLeft*100, Color.red);
+		Debug.DrawRay(pos, dirHighRight*100, Color.red);
+		Debug.DrawRay(pos, dirHighLeft*100, Color.red);
+		Debug.DrawRay(pos, dirLowRight*100, Color.red);
+		Debug.DrawRay(pos, dirLowLeft*100, Color.red);
 
 		//updating if something has been checked
 		// For now, just keeping one hit object as we don't use it outside hte
 		// if
 		RaycastHit hit;
 
-
-		if (Physics.Raycast(camVector,out hit,Mathf.Infinity)){
-			print ("something has been hit Forward");
+		// I'm keeping the explicit variables camVectorX in case we later do specific behavior for each ray.
+		// However, currently we want to loop over all the rays so I'm making an inline Ray[]
+		foreach (Ray camRay in new Ray[] {
+			camVector, camVectorHigh, camVectorLow, camVectorRight, 
+			camVectorLeft, camVectorHighRight, camVectorHighLeft, camVectorLowRight, camVectorLowLeft}) {
+			if (Physics.Raycast(camRay, out hit, Mathf.Infinity)) {
+				objectsHit.Add(hit.transform.gameObject.name);
+			}
 		}
-		if (Physics.Raycast(camVectorHigh,out hit,Mathf.Infinity)){
-			print ("something has been hit High");
-		}
-		if (Physics.Raycast(camVectorLow,out hit,Mathf.Infinity)){
-			print ("something has been hit Low");
-		}
-		if (Physics.Raycast(camVectorRight,out hit,Mathf.Infinity)){
-			print ("something has been hit Right");
-		}
-		if (Physics.Raycast(camVectorLeft,out hit,Mathf.Infinity)){
-			print ("something has been hit Left");
-		}
-		if (Physics.Raycast(camVectorHighRight,out hit,Mathf.Infinity)){
-			print ("something has been hit High Right");
-		}
-		if (Physics.Raycast(camVectorHighLeft,out hit,Mathf.Infinity)){
-			print ("something has been hit High Left");
-		}
-		if (Physics.Raycast(camVectorLowRight,out hit,Mathf.Infinity)){
-			print ("something has been hit Low Right");
-		}
-		if (Physics.Raycast(camVectorLowLeft,out hit,Mathf.Infinity)){
-			print ("something has been hit Low Left");
-		}
+		return objectsHit.ToArray ().Distinct ().Cast<string> ();
 	}
 }
---
+++ Patient/Assets/Scripts/StareTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StareTrigger : Trigger {
+	string targetName;
+	bool stareTriggered;
+
+	public StareTrigger(GameObject gameObject, string targetName) {
+		this.targetName = targetName;
+		ObjectStare objectStare = gameObject.GetComponent<ObjectStare> ();
+		if (objectStare == null) {
+			// TODO: maybe make a PatientException??
+			throw new UnityException("objectStare missing!!!");
+		}
+		stareTriggered = false;
+		objectStare.OnStare += OnStare;
+	}
+
+	public override bool IsTriggered() {
+		return stareTriggered;
+	}
+
+	private void OnStare(string objectName) {
+		if (objectName == targetName) {
+			stareTriggered = true;
+		}
+	}
+}
