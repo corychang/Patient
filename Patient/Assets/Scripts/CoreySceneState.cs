fe90492897a11e8e7d0b258a064b5c92d918e420
csobel <Corey@Coreys-MBP.wv.cc.cmu.edu>
2014-12-04 23:04:48 -0500

    saving scene state

---
+++ Patient/Assets/Scripts/CoreySceneState.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CoreySceneState : GameStateManager {
+
+	/**
+	 * Creates the gameState for the scene
+	 */
+	protected override IList<GameState> GetGameStatesList() {
+
+		//Gets the Dialog object from the camera for the dialog action
+		DialogManager dialog = Camera.mainCamera.GetComponent<DialogManager>();
+		GameObject patient = GameObject.Find ("patient");
+
+		SoundManager soundManager = GameObject.Find ("SoundManager").GetComponent<SoundManager> ();
+		GameObject obj = GameObject.FindGameObjectWithTag ("MainCamera");
+		if (obj == null) {
+			Debug.LogError ("couldn't find Main Camera");
+		}
+		soundManager.addSound (new Sound (obj, "Assets/Sounds/backgroundMusic.mp3", "background"));
+		soundManager.addSound (new Sound (obj, "Assets/Sounds/crowd-talking-1.mp3", "chatter"));
+		soundManager.addSound (new Sound (obj, "Assets/Sounds/sadMusic.mp3", "sadMusic"));
+
+
+		ActionRunner scene3HallucinateAction = new ParallelAction(
+			new List<ActionRunner>() {
+				new CameraInvertAction(),
+				new LightPulseAction() //TODO: Corey also sound affect
+			}
+		);
+		GameState scene3Hallucinate = new GameState (
+			"scene3Hallucinate",
+			new Dictionary<Trigger, string> {
+			{new ShakeTrigger(patient), "scene3Rose"}
+			},
+			scene3HallucinateAction
+		);
+		GameState scene3Rose = new GameState (
+			"scene3Rose",
+			new Dictionary<Trigger, string>(),
+			new DialogAction("rose", 2, dialog),
+			scene3HallucinateAction
+			);
+	
+		return new List<GameState> {
+			scene3Hallucinate,
+			scene3Rose
+		};
+	}
+}
