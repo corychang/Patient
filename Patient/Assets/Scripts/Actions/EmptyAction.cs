00f183f39e03259de12b55a950524c24f434cd8e
Corey Sobel <corey.sobel@gmail.com>
2014-12-05 01:17:40 -0500

    fixed up scene 3

---
+++ Patient/Assets/Scripts/Actions/EmptyAction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmptyAction : ActionRunner {
+	
+	public override void Start () {
+		done ();
+	}
+
+	public override void Interrupt () {}
+}
