bc898a4d213ddb357cdc2e08d9544d2a29fa2f62
csobel <Corey@Coreys-MBP.wv.cc.cmu.edu>
2014-12-04 21:39:45 -0500

    Added LightPulse code

---
+++ Patient/Assets/Scripts/LightPulse.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightPulse : MonoBehaviour {
+	private bool shouldPulse;
+	public bool ShouldPulse {
+		get { return shouldPulse; }
+		set { 
+			shouldPulse = value;
+			if (!shouldPulse) {
+				surrealLight.spotAngle = 1;
+			}
+		}
+	}
+
+	private Light surrealLight;
+	private int deltaAngle;
+
+	// Use this for initialization
+	void Start () {
+		deltaAngle = 20;
+		surrealLight = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shouldPulse) {
+			if (surrealLight.spotAngle + deltaAngle < 1 || 
+			    surrealLight.spotAngle + deltaAngle > 179) {
+				deltaAngle *= -1;
+			}
+			surrealLight.spotAngle += deltaAngle;
+		}
+	}
+}
---
+++ Patient/Assets/Scripts/LightPulseAction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightPulseAction : ActionRunner {
+	private bool finished;
+	private LightPulse lightPulse;
+
+	public LightPulseAction() {
+		finished = false;
+		lightPulse = GameObject.Find ("SurrealLight").GetComponent<LightPulse> ();
+	}
+
+	public override void Start() {
+		lightPulse.ShouldPulse = true;
+	}
+
+	public override void Interrupt() {
+		lightPulse.ShouldPulse = false;
+		finished = true;
+	}
+
+	public override bool IsFinished() {
+		return finished;
+	}
+}
