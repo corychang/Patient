30ec6c6244596de752c72c18e1bfe58042c1cd8a
Pratik Prakash <pratikprakash1@yahoo.com>
2014-12-05 15:24:34 -0500

    Added the credits scene

---
+++ Patient/Assets/Scripts/CreditScroller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditScroller : MonoBehaviour {
+
+	/* Public Variables    */
+	public GameObject camera;
+	public int speed;
+	public string level;
+
+	//Update is called once per frame
+	void Update () {
+		camera.transform.Translate (Vector3.down * Time.deltaTime * speed);
+
+	}
+
+	IEnumerator waitFor() {
+
+		if (!level.Equals("null")) {
+			yield return new WaitForSeconds (20);
+			Application.LoadLevel (level);
+		}
+	}
+
+}
