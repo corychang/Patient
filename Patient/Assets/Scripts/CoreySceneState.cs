0ffad881d7f10af169e1c73d22e0cc5f0148a2b7
Corey Sobel <corey.sobel@gmail.com>
2014-12-05 00:05:28 -0500

    saving CoreySceneState

--- Patient/Assets/Scripts/CoreySceneState.cs
+++ Patient/Assets/Scripts/CoreySceneState.cs
@@ -21,12 +21,14 @@ public class CoreySceneState : GameStateManager {
 		soundManager.addSound (new Sound (obj, "Assets/Sounds/backgroundMusic.mp3", "background"));
 		soundManager.addSound (new Sound (obj, "Assets/Sounds/crowd-talking-1.mp3", "chatter"));
 		soundManager.addSound (new Sound (obj, "Assets/Sounds/sadMusic.mp3", "sadMusic"));
+		soundManager.addSound (new Sound (obj, "Assets/Sounds/surreal_sound1.mp3", "surreal1"));
 
 
 		ActionRunner scene3HallucinateAction = new ParallelAction(
 			new List<ActionRunner>() {
 				new CameraInvertAction(),
-				new LightPulseAction() //TODO: Corey also sound affect
+				new LightPulseAction(), //TODO: Corey also sound affect
+				new SoundAction("surreal1", true)
 			}
 		);
 		GameState scene3Hallucinate = new GameState (
@@ -36,16 +38,55 @@ public class CoreySceneState : GameStateManager {
 			},
 			scene3HallucinateAction
 		);
+
+		ActionRunner scene3RoseAction = new SequentialAction (
+			new List<ActionRunner>(){
+				new DialogAction("Whoa, what are you doing?", 3, dialog),
+				new DialogAction("You okay?", 2, dialog),				
+				new DialogAction("I hope the flower Mom brought actually works", 4, dialog),
+				new DialogAction("I mean, it does mean \"get-well-soon\"", 3, dialog),
+			}
+		);
 		GameState scene3Rose = new GameState (
 			"scene3Rose",
-			new Dictionary<Trigger, string>(),
-			new DialogAction("rose", 2, dialog),
+			new Dictionary<Trigger, string>() {
+			{new StareTrigger(patient, "rose"), "scene3Story"}
+			},	
+			scene3RoseAction,
 			scene3HallucinateAction
 			);
+
+
+		ActionRunner scene3StoryAction = new SequentialAction (new List<ActionRunner> (){
+			new DialogAction("Sibling: They weren't originally blue; they were red.", 4, dialog),
+			new DialogAction("Sibling: Some people wanted to clone roses to see if they could breed them " +
+			                 "true and get a reliable variety they could stock and sell.", 10, dialog),
+			new DialogAction("Sibling: For some reason, " +
+			                 "they got brown flowers instead of the red ones they wanted so they were about to stop " +
+			                 "the funding, but they finally tried cloning the brown roses.", 8, dialog),
+			new DialogAction("Sibling: The next clones were blue " +
+			                 "and bred true enough for commercial purposes.", 5, dialog),
+			new DialogAction("Sibling: The thing is, everyone wasn't too bothered " +
+			"about roses being cloned, but when they moved the project to animals from roses, " +
+			"that was when it went to hell.", 10, dialog),
+			new DialogAction("Sibling: See, after the whole thing with the roses, there were ideas" +
+			                 "to fix the underpopulation problem with cloning, but there was a crowd of people against" +
+			                 "it.", 10, dialog),
+			new DialogAction("Sibling: It was risky and unreliable, actually, the whole roses debacle was really a lucky bit.", 
+			                 10, dialog),
+			new DialogAction("Sibling: Mom is against it, as a matter of fact — it's why you two got into fights.", 12, dialog)
+		});
+
+		GameState scene3Story = new GameState (
+			"scene3Story",
+			new Dictionary<Trigger, string>(),
+			scene3StoryAction
+		);
 	
 		return new List<GameState> {
 			scene3Hallucinate,
-			scene3Rose
+			scene3Rose,
+			scene3Story
 		};
 	}
 }
